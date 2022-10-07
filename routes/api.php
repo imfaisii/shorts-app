6ef29721bc4da2687e38ae92aafa16fde2c7a6b2
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2022-10-07 15:54:01 +0500

    l9

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('test', function () {
+    return [
+        "data" => [
+            "country" => [
+                "name" => "Egypt",
+                "lat" => "1.0",
+                "long" => "1.0",
+                "quotes" => ["�Egypt�is not a country we live in but a country that lives within us. �"],
+            ],
+            "short" => [
+                "link" => request()->host() . "/videos/UK/Ashdown%20forest%20uk.mp4",
+                "thumbnail" => request()->host() . "/thumbnails/UK/Ashdown%20forest%20uk0.png",
+            ],
+        ]
+    ];
+});
